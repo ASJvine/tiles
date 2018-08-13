bf65cfb7e6e1d015c26db089f5bae782359c8d46
Alexandre Serra Jaumot <aserrajaumot@gmail.com>
2018-08-13 10:42:14 +0200

    Basic react setup

---
+++ README.md
@@ -0,0 +1,5 @@
+install the last version of node 10.8.0
+
+https://github.com/tj/n
+
+
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: './src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['eslint-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
